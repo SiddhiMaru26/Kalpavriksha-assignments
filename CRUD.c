cae2a8d62989a496bec2033aa6fbcd791140d16f
Siddhi Maroo <siddhimaru032@gmail.com>
2025-10-08 22:48:30 +0530

    Update CRUD.c

--- CRUD.c
+++ CRUD.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdbool.h>
 
 #define FILE_NAME "users.txt"
 
@@ -11,37 +12,37 @@ struct User {
     int age;
 };
 
-int idExists(int id) {
+bool idExists(int id) {
     struct User existingUser;
     FILE *fp = fopen(FILE_NAME, "r");
-    if (!fp) return 0;
+    if (!fp) return false;
     while (fscanf(fp, "%d %s %d", &existingUser.id, existingUser.name, &existingUser.age) == 3) 
     {
         if (existingUser.id == id) 
         {
             fclose(fp);
-            return 1;
+            return true;
         }
     }
     fclose(fp);
-    return 0;
+    return false;
 }
 
-int isValidName(char *name) 
+bool isValidName(char *name) 
 {
-    if (strlen(name) == 0) return 0;
-    for (int i = 0; name[i] != '\0'; i++)
-        {
-          if (!isalpha(name[i]) && name[i] != ' ')
-            return 0;
+    if (strlen(name) == 0) return false;
+    for (int i = 0; name[i] != '\0'; i++) 
+    {
+        if (!isalpha(name[i]) && name[i] != ' ') 
+            return false;
     }
-    return 1;
+    return true;
 }
 
 void addUser() 
 {
     struct User newUser;
-    FILE *fp=fopen(FILE_NAME, "a");
+    FILE *fp = fopen(FILE_NAME, "a");
     if (!fp) 
     {
         printf("Cannot open file\n");
@@ -49,48 +50,42 @@ void addUser()
     }
 
     printf("Enter ID: ");
-    if (scanf("%d", &newUser.id) != 1)
+    if (scanf("%d", &newUser.id) != 1) 
     {
-        printf("Invalid input. ID must be numeric.\n");
+        printf("Invalid input. Enter a numeric ID.\n");
         while (getchar() != '\n');
         fclose(fp);
         return;
     }
 
-    if (newUser.id <= 0)
+    if (newUser.id <= 0) 
     {
-        printf("ID must be positive.\n");
+        printf("Invalid input. Enter a numeric ID.\n");
         fclose(fp);
         return;
     }
 
-    if (idExists(newUser.id))
+    if (idExists(newUser.id)) 
     {
-        printf("This ID already exists. Choose a different one.\n");
+        printf("Error: A user with ID %d already exists. Please enter a unique ID.\n", newUser.id);
         fclose(fp);
         return;
     }
 
     printf("Enter Name: ");
-    scanf(" %[^\n]", newUser.name); 
-    if (!isValidName(newUser.name)) {
-        printf("Invalid name. Only letters and spaces allowed.\n");
+    scanf(" %[^\n]", newUser.name);
+    if (!isValidName(newUser.name)) 
+    {
+        printf("Invalid name. Name must contain only letters and spaces.\n");
         fclose(fp);
         return;
     }
 
     printf("Enter Age: ");
-    if (scanf("%d", &newUser.age) != 1)
+    if (scanf("%d", &newUser.age) != 1 || newUser.age < 0 || newUser.age > 100) 
     {
-        printf("Invalid input. Age must be numeric.\n");
+        printf("Age must be between 0 and 100.\n");
         while (getchar() != '\n');
-        fclose(fp);
-        return;
-    }
-
-    if (newUser.age < 0 || newUser.age > 120) 
-    {
-        printf("Age must be between 0 and 120.\n");
         fclose(fp);
         return;
     }
@@ -103,21 +98,21 @@ void addUser()
 void showUsers() 
 {
     struct User existingUser;
-    FILE *fp=fopen(FILE_NAME, "r");
-    if (!fp)
+    FILE *fp = fopen(FILE_NAME, "r");
+    if (!fp) 
     {
         printf("No users found\n");
         return;
     }
     printf("\n--- Users ---\n");
-    while (fscanf(fp, "%d %s %d", &existingUser.id, existingUser.name, &existingUser.age) == 3)
-        {
+    while (fscanf(fp, "%d %s %d", &existingUser.id, existingUser.name, &existingUser.age) == 3) 
+    {
         printf("ID:%d Name:%s Age:%d\n", existingUser.id, existingUser.name, existingUser.age);
     }
     fclose(fp);
 }
 
-void updateUser()
+void updateUser() 
 {
     struct User currentUser;
     int id, found = 0;
@@ -126,6 +121,8 @@ void updateUser()
     if (!fp || !temp) 
     {
         printf("Cannot open file\n");
+        if(fp) fclose(fp);
+        if(temp) fclose(temp);
         return;
     }
 
@@ -154,22 +151,27 @@ void updateUser()
     else printf("User not found\n");
 }
 
-void deleteUser() {
+void deleteUser() 
+{
     struct User currentUser;
-    int id, found=0;
-    FILE *fp=fopen(FILE_NAME, "r");
-    FILE *temp=fopen("temp.txt", "w");
-    if (!fp || !temp) {
+    int id, found = 0;
+    FILE *fp = fopen(FILE_NAME, "r");
+    FILE *temp = fopen("temp.txt", "w");
+    if (!fp || !temp) 
+    {
         printf("Cannot open file\n");
+        if(fp) fclose(fp);
+        if(temp) fclose(temp);
         return;
     }
 
     printf("Enter ID to delete: ");
     scanf("%d", &id);
 
-    while (fscanf(fp, "%d %s %d", &currentUser.id, currentUser.name, &currentUser.age) == 3)
+    while (fscanf(fp, "%d %s %d", &currentUser.id, currentUser.name, &currentUser.age) == 3) 
+    {
+        if (currentUser.id == id) 
         {
-        if (currentUser.id == id) {
             found = 1;
             continue;
         }
@@ -185,9 +187,11 @@ void deleteUser() {
     else printf("User not found\n");
 }
 
-int main() {
+int main() 
+{
     int choice;
-    while (1) {
+    while (1) 
+    {
         printf("\n--- Menu ---\n");
         printf("1.Add User\n2.Show Users\n3.Update User\n4.Delete User\n5.Exit\n");
         printf("Enter choice: ");
