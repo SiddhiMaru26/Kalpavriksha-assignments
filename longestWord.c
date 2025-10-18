9907878bf0c1d10850279be4f93c52f1f3b0229e
Siddhi Maroo <siddhimaru032@gmail.com>
2025-10-18 16:37:37 +0530

    Refactor main function for clarity and consistency

--- longestWord.c
+++ longestWord.c
@@ -3,7 +3,8 @@
 #define MAX_WORDS 100
 #define MAX_LENGTH 50
 
-int main() {
+int main() 
+{
     int size;
     printf("Enter number of words: ");
     scanf("%d", &size);
@@ -12,22 +13,26 @@ int main() {
     char (*pointer)[MAX_LENGTH] = words; 
 
     printf("Enter words:\n");
-    for (int index = 0; index< size; index++) {
+    for (int index = 0; index< size; index++) 
+    {
         scanf("%s", *(pointer + index)); 
     }
 
     char *longestWord = *(pointer + 0);
     int maxLength = 0;
 
-    for (int index = 0; index < size; index++) {
+    for (int index = 0; index < size; index++) 
+    {
         char *wordPtr = *(pointer + index);
         int length = 0;
 
-        while (*(wordPtr + length) != '\0') {
+        while (*(wordPtr + length) != '\0') 
+        {
             length++;
         }
 
-        if (length > maxLength) {
+        if (length > maxLength) 
+        {
             maxLength = length;
             longestWord = *(pointer + index);
         }
