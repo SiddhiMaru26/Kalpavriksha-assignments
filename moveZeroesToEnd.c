1f9e9a881530b8c1640dfe6223f396cae75f7fd8
Siddhi Maroo <siddhimaru032@gmail.com>
2025-10-18 16:35:28 +0530

    Refactor to move zeros in an integer array
    Updated the program to read an array of integers instead of words and move all zeros to the end of the array.

--- moveZeroesToEnd.c
+++ moveZeroesToEnd.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 
-#define MAX_WORDS 100
-#define MAX_LENGTH 50
-
 int main() {
     int size;
-    printf("Enter number of words: ");
+    printf("Enter number of elements: ");
     scanf("%d", &size);
 
-    char words[MAX_WORDS][MAX_LENGTH];
-    char (*pointer)[MAX_LENGTH] = words; 
-
-    printf("Enter words:\n");
-    for (int index = 0; index< size; index++) {
-        scanf("%s", *(pointer + index)); 
+    int array[size];
+    printf("Enter array elements: ");
+    for (int index = 0; index < size; index++) 
+    {
+        scanf("%d", array + index); 
     }
 
-    char *longestWord = *(pointer + 0);
-    int maxLength = 0;
-
-    for (int index = 0; index < size; index++) {
-        char *wordPtr = *(pointer + index);
-        int length = 0;
-
-        while (*(wordPtr + length) != '\0') {
-            length++;
-        }
-
-        if (length > maxLength) {
-            maxLength = length;
-            longestWord = *(pointer + index);
+    int position = 0; 
+    for (int index = 0; index < size; index++) 
+    {
+        if (*(array + index) != 0) 
+        {
+            *(array + position) = *(array + index);
+            position++;
         }
     }
 
-    printf("The longest word is: %s\n", longestWord);
+    for (int index = position; index < size; index++) 
+    {
+        *(array + index) = 0;
+    }
+
+    printf("Array after moving zeros: ");
+    for (int index = 0; index < size; index++) 
+    {
+        printf("%d ", *(array + index));
+    }
+    printf("\n");
 
     return 0;
 }
