4669e7f3980a46719139ed91a44be7bacfe80c4f
Siddhi Maroo <siddhimaru032@gmail.com>
2025-12-21 10:57:48 +0530

    Refactor message queue implementation for clarity

--- IPCAssignment/MessageQueueIPCMechanism/messageQueueIPC.c
+++ IPCAssignment/MessageQueueIPCMechanism/messageQueueIPC.c
@@ -1,55 +1,114 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 
-struct MessageQueue
+#define MAX_ELEMENTS 100
+
+struct MsgQueue
 {
     long type;
-    int array[100];
+    int array[MAX_ELEMENTS];
     int numElements;
 };
 
-int main()
+int getIntegerInput()
 {
-    key_t queueKey = 1234;
-    int queueId = msgget(queueKey, 0666 | IPC_CREAT);
-    struct MessageQueue sendMsg;
-
-    printf("Enter number of elements: ");
-    scanf("%d", &sendMsg.numElements);
-
-    for (int index = 0; index < sendMsg.numElements; index++)
+    int value;
+    while (1)
     {
-        printf("Enter element %d: ", index + 1);
-        scanf("%d", &sendMsg.array[index]);
-    }
-
-    sendMsg.type = 1;
-    msgsnd(queueId, &sendMsg, sizeof(sendMsg) - sizeof(long), 0);
-
-    struct MessageQueue receiveMsg;
-    msgrcv(queueId, &receiveMsg, sizeof(receiveMsg) - sizeof(long), 1, 0);
-
-    for (int pass = 0; pass < receiveMsg.numElements - 1; pass++)
-    {
-        for (int index = 0; index < receiveMsg.numElements - 1 - pass; index++)
+        if (scanf("%d", &value) != 1)
         {
-            if (receiveMsg.array[index] > receiveMsg.array[index + 1])
+            while (getchar() != '\n');
+        }
+        else
+        {
+            if (getchar() != '\n')
             {
-                int tempValue = receiveMsg.array[index];
-                receiveMsg.array[index] = receiveMsg.array[index + 1];
-                receiveMsg.array[index + 1] = tempValue;
+                while (getchar() != '\n');
+            }
+            else
+            {
+                return value;
             }
         }
     }
+}
 
-    printf("Sorted Array: ");
-    for (int index = 0; index < receiveMsg.numElements; index++)
+int readArrayFromUser(struct MsgQueue *message)
+{
+    message->numElements = getIntegerInput();
+    if (message->numElements <= 0 || message->numElements > MAX_ELEMENTS)
     {
-        printf("%d ", receiveMsg.array[index]);
+        return -1;
+    }
+    for (int index = 0; index < message->numElements; index++)
+    {
+        message->array[index] = getIntegerInput();
+    }
+    return message->numElements;
+}
+
+void sortArray(int *array, int elementCount)
+{
+    for (int pass = 0; pass < elementCount - 1; pass++)
+    {
+        for (int index = 0; index < elementCount - 1 - pass; index++)
+        {
+            if (array[index] > array[index + 1])
+            {
+                int temp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = temp;
+            }
+        }
+    }
+}
+
+void sendArrayToQueue(int queueId, struct MsgQueue *message)
+{
+    message->type = 1;
+    msgsnd(queueId, message, sizeof(struct MsgQueue) - sizeof(long), 0);
+}
+
+void receiveArrayFromQueue(int queueId, struct MsgQueue *message)
+{
+    msgrcv(queueId, message, sizeof(struct MsgQueue) - sizeof(long), 1, 0);
+}
+
+void displayArray(int *array, int elementCount)
+{
+    for (int index = 0; index < elementCount; index++)
+    {
+        printf("%d ", array[index]);
     }
     printf("\n");
+}
+
+void executeMessageQueueSorting()
+{
+    key_t queueKey = 1234;
+    int queueId = msgget(queueKey, 0666 | IPC_CREAT);
+
+    struct MsgQueue message;
+    if (readArrayFromUser(&message) == -1)
+    {
+        return;
+    }
+
+    sendArrayToQueue(queueId, &message);
+
+    struct MsgQueue receivedMessage;
+    receiveArrayFromQueue(queueId, &receivedMessage);
+
+    sortArray(receivedMessage.array, receivedMessage.numElements);
+    displayArray(receivedMessage.array, receivedMessage.numElements);
 
     msgctl(queueId, IPC_RMID, NULL);
+}
+
+int main()
+{
+    executeMessageQueueSorting();
     return 0;
 }
