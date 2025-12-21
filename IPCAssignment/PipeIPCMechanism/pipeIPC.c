b96b5f0e345e34659c4da65837f6fd9d20b4dfdb
Siddhi Maroo <siddhimaru032@gmail.com>
2025-12-21 10:56:46 +0530

    Refactor IPC mechanism with enhanced input and sorting
    Refactored IPC mechanism to improve input handling and sorting.

--- IPCAssignment/PipeIPCMechanism/pipeIPC.c
+++ IPCAssignment/PipeIPCMechanism/pipeIPC.c
@@ -1,57 +1,122 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
-int main()
+#define MAX_ELEMENTS 100
+
+int getIntegerInput()
+{
+    int value;
+    while (1)
+    {
+        if (scanf("%d", &value) != 1)
+        {
+            while (getchar() != '\n');
+        }
+        else
+        {
+            if (getchar() != '\n')
+            {
+                while (getchar() != '\n');
+            }
+            else
+            {
+                return value;
+            }
+        }
+    }
+}
+
+int readArrayFromUser(int *array)
+{
+    int elementCount;
+    elementCount = getIntegerInput();
+    if (elementCount <= 0 || elementCount > MAX_ELEMENTS)
+    {
+        return -1;
+    }
+    for (int index = 0; index < elementCount; index++)
+    {
+        array[index] = getIntegerInput();
+    }
+    return elementCount;
+}
+
+void sortArray(int *array, int elementCount)
+{
+    for (int pass = 0; pass < elementCount - 1; pass++)
+    {
+        for (int index = 0; index < elementCount - 1 - pass; index++)
+        {
+            if (array[index] > array[index + 1])
+            {
+                int temp = array[index];
+                array[index] = array[index + 1];
+                array[index + 1] = temp;
+            }
+        }
+    }
+}
+
+void parentProcessActions(int pipeDescriptors[2], int *array, int elementCount)
+{
+    close(pipeDescriptors[0]);
+    write(pipeDescriptors[1], array, elementCount * sizeof(int));
+    close(pipeDescriptors[1]);
+}
+
+void childProcessActions(int pipeDescriptors[2], int elementCount)
+{
+    close(pipeDescriptors[1]);
+    int receivedArray[MAX_ELEMENTS];
+    read(pipeDescriptors[0], receivedArray, elementCount * sizeof(int));
+    close(pipeDescriptors[0]);
+    sortArray(receivedArray, elementCount);
+    for (int index = 0; index < elementCount; index++)
+    {
+        printf("%d ", receivedArray[index]);
+    }
+    printf("\n");
+}
+
+void executePipeBasedSorting()
 {
     int pipeDescriptors[2];
     pipe(pipeDescriptors);
 
-    int numElements;
-    printf("Enter number of elements: ");
-    scanf("%d", &numElements);
-
-    int inputArray[numElements];
-    for (int index = 0; index < numElements; index++)
+    int elementCount;
+    elementCount = getIntegerInput();
+    if (elementCount <= 0 || elementCount > MAX_ELEMENTS)
     {
-        printf("Enter element %d: ", index + 1);
-        scanf("%d", &inputArray[index]);
+        return;
     }
 
-    int processId = fork();
-
-    if (processId == 0)
+    int userArray[MAX_ELEMENTS];
+    for (int index = 0; index < elementCount; index++)
     {
-        close(pipeDescriptors[0]);
-        write(pipeDescriptors[1], inputArray, sizeof(inputArray));
-        close(pipeDescriptors[1]);
+        userArray[index] = getIntegerInput();
+    }
+
+    pid_t processId = fork();
+
+    if (processId < 0)
+    {
+        return;
+    }
+    else if (processId == 0)
+    {
+        parentProcessActions(pipeDescriptors, userArray, elementCount);
     }
     else
     {
-        close(pipeDescriptors[1]);
-        int sortedArray[numElements];
-        read(pipeDescriptors[0], sortedArray, sizeof(sortedArray));
-        close(pipeDescriptors[0]);
-
-        for (int pass = 0; pass < numElements - 1; pass++)
-        {
-            for (int index = 0; index < numElements - 1 - pass; index++)
-            {
-                if (sortedArray[index] > sortedArray[index + 1])
-                {
-                    int tempValue = sortedArray[index];
-                    sortedArray[index] = sortedArray[index + 1];
-                    sortedArray[index + 1] = tempValue;
-                }
-            }
-        }
-
-        printf("Sorted Array: ");
-        for (int index = 0; index < numElements; index++)
-        {
-            printf("%d ", sortedArray[index]);
-        }
-        printf("\n");
+        wait(NULL);
+        childProcessActions(pipeDescriptors, elementCount);
     }
+}
 
+int main()
+{
+    executePipeBasedSorting();
     return 0;
 }
