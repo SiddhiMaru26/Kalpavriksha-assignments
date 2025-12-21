daae2f89dce08b0c8d003ff45c0dd495a8041ff4
Siddhi Maroo <siddhimaru032@gmail.com>
2025-12-21 10:53:40 +0530

    Refactor file IPC mechanism with improved input handling

--- IPCAssignment/FileIPCMechanism/fileIPC.c
+++ IPCAssignment/FileIPCMechanism/fileIPC.c
@@ -1,55 +1,154 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
-int main()
+#define MAX_ELEMENTS 100
+#define DATA_FILE "data.txt"
+
+int getIntegerInput()
 {
-    int numElements;
-    printf("Enter number of elements: ");
-    scanf("%d", &numElements);
-
-    int inputArray[numElements];
-    for (int index = 0; index < numElements; index++)
+    int value;
+    while (1)
     {
-        printf("Enter element %d: ", index + 1);
-        scanf("%d", &inputArray[index]);
-    }
-
-    FILE *dataFile;
-    dataFile = fopen("data.txt", "w");
-    for (int index = 0; index < numElements; index++)
-    {
-        putc(inputArray[index] + '0', dataFile);
-        putc(' ', dataFile);
-    }
-    fclose(dataFile);
-
-    int sortedArray[numElements];
-    dataFile = fopen("data.txt", "r");
-    for (int index = 0; index < numElements; index++)
-    {
-        fscanf(dataFile, "%d", &sortedArray[index]);
-    }
-    fclose(dataFile);
-
-    for (int pass = 0; pass < numElements - 1; pass++)
-    {
-        for (int index = 0; index < numElements - 1 - pass; index++)
+        if (scanf("%d", &value) != 1)
         {
-            if (sortedArray[index] > sortedArray[index + 1])
+            while (getchar() != '\n');
+        }
+        else
+        {
+            if (getchar() != '\n')
             {
-                int tempValue = sortedArray[index];
-                sortedArray[index] = sortedArray[index + 1];
-                sortedArray[index + 1] = tempValue;
+                while (getchar() != '\n');
+            }
+            else
+            {
+                return value;
             }
         }
     }
+}
 
-    printf("Sorted Array: ");
-    for (int index = 0; index < numElements; index++)
+int readArrayFromUser(int *array)
+{
+    int elementCount;
+    elementCount = getIntegerInput();
+    if (elementCount <= 0 || elementCount > MAX_ELEMENTS)
     {
-        printf("%d ", sortedArray[index]);
+        return -1;
+    }
+    for (int index = 0; index < elementCount; index++)
+    {
+        array[index] = getIntegerInput();
+    }
+    return elementCount;
+}
+
+void printArray(int *array, int elementCount)
+{
+    for (int index = 0; index < elementCount; index++)
+    {
+        printf("%d ", array[index]);
     }
     printf("\n");
+}
 
+void saveArrayToFile(int *array, int elementCount)
+{
+    FILE *file = fopen(DATA_FILE, "w");
+    if (!file)
+    {
+        return;
+    }
+    fprintf(file, "%d\n", elementCount);
+    for (int index = 0; index < elementCount; index++)
+    {
+        fprintf(file, "%d ", array[index]);
+    }
+    fclose(file);
+}
+
+int loadArrayFromFile(int *array)
+{
+    FILE *file = fopen(DATA_FILE, "r");
+    if (!file)
+    {
+        return 0;
+    }
+    int elementCount;
+    fscanf(file, "%d", &elementCount);
+    for (int index = 0; index < elementCount; index++)
+    {
+        fscanf(file, "%d", &array[index]);
+    }
+    fclose(file);
+    return elementCount;
+}
+
+void bubbleSortArray(int *array, int elementCount)
+{
+    for (int index = 0; index < elementCount - 1; index++)
+    {
+        for (int innerIndex = 0; innerIndex < elementCount - index - 1; innerIndex++)
+        {
+            if (array[innerIndex] > array[innerIndex + 1])
+            {
+                int temp = array[innerIndex];
+                array[innerIndex] = array[innerIndex + 1];
+                array[innerIndex + 1] = temp;
+            }
+        }
+    }
+}
+
+void parentProcessActions(int *array, int elementCount)
+{
+    saveArrayToFile(array, elementCount);
+}
+
+void childProcessActions()
+{
+    int childArray[MAX_ELEMENTS];
+    int childCount = loadArrayFromFile(childArray);
+    bubbleSortArray(childArray, childCount);
+    saveArrayToFile(childArray, childCount);
+}
+
+void executeFileBasedIPC()
+{
+    int userArray[MAX_ELEMENTS];
+    int elementCount;
+
+    elementCount = readArrayFromUser(userArray);
+    if (elementCount == -1)
+    {
+        return;
+    }
+
+    parentProcessActions(userArray, elementCount);
+
+    pid_t processId = fork();
+
+    if (processId < 0)
+    {
+        return;
+    }
+    else if (processId == 0)
+    {
+        childProcessActions();
+        exit(0);
+    }
+    else
+    {
+        wait(NULL);
+        int sortedArray[MAX_ELEMENTS];
+        int sortedCount = loadArrayFromFile(sortedArray);
+        printArray(sortedArray, sortedCount);
+    }
+}
+
+int main()
+{
+    executeFileBasedIPC();
     return 0;
 }
